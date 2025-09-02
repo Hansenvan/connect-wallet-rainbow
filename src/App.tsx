e86d1802d422b46ce8270701b0342183db1fd3d3
Hansenvan <96397030+Hansenvan@users.noreply.github.com>
2025-09-01 21:13:49 -0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
 
-import React from "react";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { mainnet } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import React from 'react';
 
-const App = () => {
+const { chains, publicClient } = configureChains(
+  [mainnet],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'RainbowKit Wallet Connect',
+  projectId: 'YOUR_PROJECT_ID',
+  chains
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+});
+
+function App() {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>RainbowKit Wallet Connect</h1>
-      <ConnectButton />
-    </div>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: 100 }}>
+          <h1>RainbowKit Wallet Connect</h1>
+        </div>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
-};
+}
 
 export default App;
